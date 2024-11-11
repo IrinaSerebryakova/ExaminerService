b21ca98e761d8b1e53f16508adff2651ebb67bea
SerebryakovaIA <ia.serebr@gmail.com>
2024-11-11 13:56:24 +0300

    Прошу проверить курсовую работу.

--- src/test/java/pro/sky/java/course2/examinerservice/ExaminerServiceImplTest.java
+++ src/test/java/pro/sky/java/course2/examinerservice/ExaminerServiceImplTest.java
@@ -1,46 +1,63 @@
 package pro.sky.java.course2.examinerservice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pro.sky.java.course2.examinerservice.exception.QuestionAlreadyAddedException;
+import pro.sky.java.course2.examinerservice.exception.QuestionAndAnswerHaveTheSameMeaningException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static pro.sky.java.course2.examinerservice.QuestionTestConstants.ANSWER;
-import static pro.sky.java.course2.examinerservice.QuestionTestConstants.QUESTION;
-class ExaminerServiceImplTest {
-    private final List<Question> examinerQuestions = new ArrayList<>();
-    private final QuestionService questionService;
-    private final ExaminerServiceImpl examinerService = new ExaminerServiceImpl(questionService);
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static pro.sky.java.course2.examinerservice.QuestionTestConstants.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ExaminerServiceImplTest {
+  @Mock
+  private QuestionService questionServiceMock = mock(QuestionService.class);
+  @InjectMocks
+  private ExaminerService examinerServiceMock = mock(ExaminerService.class);
+
+
     @Test
-    void getQuestions() {
+    void shouldGetFiveQuestions() {
+      when(examinerServiceMock.getQuestions(5)).thenReturn(QUESTION_PROGRAMMING_DATA);
+
+      assertEquals(QUESTION_PROGRAMMING_DATA, examinerServiceMock.getQuestions(5));
+    }
+    @Test
+    public void shouldFindAddedQuestion() {
+      Question actual = questionServiceMock.add(QUESTION, ANSWER);
+      when(questionServiceMock.add(actual)).thenReturn(actual);
+
+    assertEquals(actual, questionServiceMock.add(actual));
     }
 
     @Test
-    public void shouldAddQuestionIfDoNotExist() {
-        Question actual = examinerServiceTest.add(QUESTION, ANSWER);
-
-        assertEquals(actual.getQuestion(), QUESTION);
-        assertEquals(actual.getAnswer(), ANSWER);
+    public void shouldRemoveQuestion() {
+      Question actual = questionServiceMock.add(QUESTION, ANSWER);
+      when(questionServiceMock.remove(actual)).thenReturn(actual);
+      assertEquals(actual, questionServiceMock.remove(actual));
     }
 
     @Test
-    void testAdd() {
+    public void shouldGetAllCollectionOfQuestions() {
+      when(questionServiceMock.getAll()).thenReturn(QUESTION_PROGRAMMING_DATA);
+
+      assertEquals(QUESTION_PROGRAMMING_DATA, questionServiceMock.getAll());
     }
 
-    @Test
-    void remove() {
-    }
 
     @Test
-    void getAll() {
-    }
-
-    @Test
-    void getRandomQuestion() {
-    }
-
-    @Test
-    void check() {
+    public void shouldGetRandomQuestion() {
+      Question actual = questionServiceMock.add(QUESTION, ANSWER);
+      when(questionServiceMock.getRandomQuestion()).thenReturn(actual);
+      assertEquals(actual, questionServiceMock.getRandomQuestion());
     }
 }
